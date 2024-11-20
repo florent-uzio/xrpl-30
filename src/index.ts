a29f5a33f9cd3e63d5cbdf0edfae1989d34a711e
Florent Uzio <fuzio@ripple.com>
2024-11-20 21:21:31 +0400

    fifth video

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,10 @@ import { sendPayment } from "./1-send-payment";
 import { sendPaymentAndWait } from "./2-send-payment-and-wait";
 import { createWallet } from "./3-create-wallet";
 import { balanceAndTransactions } from "./4-balance-and-txn";
+import { issueToken } from "./5-issue-token";
 
 // sendPayment()
 // sendPaymentAndWait()
 // createWallet();
 // balanceAndTransactions();
+// issueToken();
